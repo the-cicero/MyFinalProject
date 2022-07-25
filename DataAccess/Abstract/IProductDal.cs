65c5e23ae440d8d6017c09e10dc52b32a160a9b1
Mehmet Arif Akçadağ <190509050@firat.edu.tr>
2022-07-25 14:11:17 +0300

    Add project files.

---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Concrete;
+
+namespace DataAccess.Abstract
+{
+    public interface IProductDal // dal - data access layer
+    {
+        List<Product> GetAll();
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+
+        List<Product> GetAllByCategory(int categoryId);
+
+
+    }
+}
